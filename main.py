c5766abd8fb11754078dafc5fc1b852dab985033
TanyaDgrChf <125532894+TanyaDgrChf@users.noreply.github.com>
2023-07-21 15:21:03 +0800

    Password handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from password_handling import encryptPassword
 
 app = Flask(__name__)
 
--- password_handling.py
+++ password_handling.py
@@ -1,8 +1,48 @@
 def hashPassword(password):
-    for i in password:
+    hashed = ''
+    for i in range(len(password)):
+        symbol = ord(password[i])
+        symbol += fibonacci(i + 1)**2
+        hashed += chr(symbol)
+    return hashed
 
+def saltPassword(password):
+    endPassword = password
+    for i in range(primes(len(password))):
+        endPassword += str(chr(i))
+    return endPassword
+
+def encryptPassword(password):
+    encrypted = password
+    for i in range(len(password)):
+        encrypted = hashPassword(saltPassword(encrypted))
+    return encrypted
 
 def fibonacci(n):
-    a = 1
-    b = 1
-    while n > 0:
+    more = 1
+    less = 1
+    temp = 0
+    if n < 3:
+        return 1
+    for i in range(n - 2):
+        temp = more
+        more += less
+        less = temp
+    return more
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def primes(n):
+    primes = []
+    number = 2
+    while len(primes) < n:
+        if is_prime(number):
+            primes.append(number)
+        number += 1
+    return primes[-1]
